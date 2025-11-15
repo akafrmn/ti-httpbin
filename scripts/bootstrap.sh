93b4aba36ca5ecfb8a52249aee2971e23805ac86
akafrmn <nils.feuerkraft@gmail.com>
2025-11-15 23:33:32 +0200

    remove np

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# CLuster used: local docker desktop with kind
+# Options: use k3d for more automation
+
+# Install Flux
+flux bootstrap github --owner=akafrmn \
+  --repository=ti-httpbin --private=false \
+  --personal=true --components-extra source-watcher \
+  --branch=develop --path=clusters/docker-desktop
+
+git pull origin develop
